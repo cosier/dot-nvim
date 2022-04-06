2b390e59e5f00dc6588eb0fa6087958f47f597b0
Bailey Cosier <bailey.cosier@gmail.com>
2022-04-06 17:54:11 +0700

    Updates

--- _init.vim
+++ _init.vim
@@ -1,6 +1,9 @@
 call plug#begin()
   " Core
 	Plug 'lambdalisue/suda.vim'
+	Plug 'yegappan/bufselect'
+	Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+	Plug 'junegunn/fzf.vim'
 	" Appearance
 	Plug 'vim-airline/vim-airline'
 	Plug 'ryanoasis/vim-devicons'
@@ -19,14 +22,11 @@ call plug#begin()
 	Plug 'thedenisnikulin/vim-cyberpunk'
 
 
-
-
 	" Utilities
 	Plug 'nvim-lua/plenary.nvim'
 	Plug 'nvim-telescope/telescope.nvim'
 
 	Plug 'sheerun/vim-polyglot'
-	"Plug 'jiangmiao/auto-pairs'
 	Plug 'ap/vim-css-color'
 	Plug 'preservim/nerdtree'
 	Plug 'kien/ctrlp.vim'
@@ -62,8 +62,8 @@ nnoremap <silent> <Leader>s :FixWhitespace<CR>
 nnoremap <silent>    <Leader>cc :Commentary<CR>
 
 " Move to previous/next
-nnoremap <silent>    <A-,> :BufferPrevious<CR>
-nnoremap <silent>    <A-.> :BufferNext<CR>
+nnoremap <silent>    <Leader>p :BufferPrevious<CR>
+nnoremap <silent>    <Leader>n :BufferNext<CR>
 " Re-order to previous/next
 nnoremap <silent>    <A-<> :BufferMovePrevious<CR>
 nnoremap <silent>    <A->> :BufferMoveNext<CR>
@@ -116,13 +116,11 @@ set cursorline
 " Nerd Tree
 let NERDTreeQuitOnOpen=1
 
-
 " Airline
 " let g:airline_theme = 'dracula'
 let g:airline_powerline_fonts = 1
 let g:airline#extensions#tabline#enabled = 1
 let g:airline#extensions#tabline#buffer_nr_show = 1
-
 
 " Searching
 if executable('ag')
@@ -131,10 +129,9 @@ endif
 
 nnoremap <C-s> :Ack!<Space>
 
-
 " Italics
-let &t_ZH = "\e[3m"
-let &t_ZR = "\e[23m"
+"let &t_ZH = "\e[3m"
+"let &t_ZR = "\e[23m"
 
 " File browser
 let NERDTreeShowHidden = 1
@@ -153,8 +150,8 @@ let g:tex_conceal = ''
 let g:vim_markdown_math = 1
 
 " Language server stuff
-let g:python3_host_prog = '/usr/local/bin/python3'
-command! -nargs=0 Prettier :call CocAction('runCommand', 'prettier.formatFile')
+"let g:python3_host_prog = '/usr/local/bin/python3'
+"command! -nargs=0 Prettier :call CocAction('runCommand', 'prettier.formatFile')
 
 " Folding
 "highlight Folded
@@ -165,53 +162,6 @@ command! -nargs=0 Prettier :call CocAction('runCommand', 'prettier.formatFile')
 "setlocal foldmethod=expr
 "setlocal foldexpr=GetPotionFold(v:lnum)
 
-function! GetPotionFold(lnum)
-  if getline(a:lnum) =~? '\v^\s*$'
-    return '-1'
-  endif
-  let this_indent = IndentLevel(a:lnum)
-  let next_indent = IndentLevel(NextNonBlankLine(a:lnum))
-  if next_indent == this_indent
-    return this_indent
-  elseif next_indent < this_indent
-    return this_indent
-  elseif next_indent > this_indent
-    return '>' . next_indent
-  endif
-endfunction
-function! IndentLevel(lnum)
-    return indent(a:lnum) / &shiftwidth
-endfunction
-function! NextNonBlankLine(lnum)
-  let numlines = line('$')
-  let current = a:lnum + 1
-  while current <= numlines
-      if getline(current) =~? '\v\S'
-          return current
-      endif
-      let current += 1
-  endwhile
-  return -2
-endfunction
-function! CustomFoldText()
-  " get first non-blank line
-  let fs = v:foldstart
-  while getline(fs) =~ '^\s*$' | let fs = nextnonblank(fs + 1)
-  endwhile
-  if fs > v:foldend
-      let line = getline(v:foldstart)
-  else
-      let line = substitute(getline(fs), '\t', repeat(' ', &tabstop), 'g')
-  endif
-  let w = winwidth(0) - &foldcolumn - (&number ? 8 : 0)
-  let foldSize = 1 + v:foldend - v:foldstart
-  let foldSizeStr = " " . foldSize . " lines "
-  let foldLevelStr = repeat("+--", v:foldlevel)
-  let expansionString = repeat(" ", w - strwidth(foldSizeStr.line.foldLevelStr))
-  return line . expansionString . foldSizeStr . foldLevelStr
-endfunction
-
-
 " Normal mode remappings
 nnoremap <C-q> :q!<CR>
 nnoremap <F4> :bd<CR>
@@ -219,7 +169,7 @@ nnoremap <F5> :NERDTreeToggle<CR>
 nnoremap <F6> :sp<CR>:terminal<CR>
 nnoremap <F10> :CocCommand tsserver.organizeImports<CR>
 
-
+nnoremap <leader>b :Buffers<CR>
 nnoremap <leader>s :w<CR>
 nnoremap <leader>n :NERDTreeFocus<CR>
 nnoremap <C-n> :NERDTree<CR>
@@ -230,9 +180,3 @@ nnoremap <C-f> :NERDTreeFind<CR>
 nnoremap <S-Tab> gT
 nnoremap <Tab> gt
 nnoremap <silent> <S-t> :tabnew<CR>
-
-" Show highlight groups
-map <F12> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
-\ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
-\ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<CR>
-
---
+++ init.lua
@@ -0,0 +1,171 @@
+------------------------------------------------------
+-- Utilities
+------------------------------------------------------
+-- Functional wrapper for mapping custom keybindings
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+
+------------------------------------------------------
+-- Packer Plugins
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Package manager
+  use { 'ibhagwan/fzf-lua', requires = { 'kyazdani42/nvim-web-devicons' }}
+
+  use { 'tom-anders/telescope-vim-bookmarks.nvim' }
+
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+  use 'tiagovla/tokyodark.nvim'
+
+   -- require('telescope').load_extension('vim_bookmarks')
+
+end)
+
+
+
+------------------------------------------------------
+-- Plug Installation
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+Plug 'yegappan/bufselect'
+Plug 'romgrk/barbar.nvim'
+Plug 'nvim-lua/plenary.nvim'
+
+
+Plug 'tpope/vim-sensible'
+Plug 'preservim/nerdtree'
+Plug 'MattesGroeger/vim-bookmarks'
+
+Plug 'vim-airline/vim-airline'
+Plug 'kyazdani42/nvim-web-devicons'
+
+-- Plug 'romgrk/barbar.nvim'
+Plug 'bronson/vim-trailing-whitespace'
+Plug 'tpope/vim-commentary'
+Plug 'valloric/matchtagalways'
+Plug 'Yggdroot/indentLine'
+
+Plug('ms-jpq/coq_nvim', { branch = 'coq'})
+Plug('ms-jpq/coq.artifacts', { branch = 'artifacts'})
+Plug('ms-jpq/coq.thirdparty', { branch = '3p'})
+
+-- Themes
+Plug 'altercation/vim-colors-solarized'
+Plug 'thedenisnikulin/vim-cyberpunk'
+Plug('dracula/vim', { as = 'dracula' })
+
+-- Utilities
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+
+Plug 'sheerun/vim-polyglot'
+Plug 'ap/vim-css-color'
+Plug 'mileszs/ack.vim'
+Plug 'preservim/nerdcommenter'
+
+-- Git
+Plug 'airblade/vim-gitgutter'
+Plug 'Shougo/unite.vim'
+
+vim.call('plug#end')
+
+vim.g.mapleader = " "
+
+------------------------------------------------------
+-- Plug Hotkey
+map("n", "<C-f>", ":NERDTree<CR>", { silent = true })
+-- map("n", "<Leader>b", ":FzfLua buffers<CR>", { silent = true })
+map("n", "<C-p>", ":FzfLua git_files<CR>", { silent = true })
+map("n", "<Leader>q", ":COQnow<CR>", { silent = true })
+map("n", "<Leader>c", ":Commentary<CR>", { silent = true })
+
+map("n", "<Leader>1", ":BufferPrevious<CR>")
+map("n", "<Leader>2", ":BufferNext<CR>")
+map("n", "<Leader>m", ":BookmarkShowAll<CR>")
+
+map("n", "<Leader>r", ":source ~/.nvim/init.lua<CR>", { silent = true })
+
+
+
+------------------------------------------------------
+-- VIM Configuration
+
+vim.cmd [[
+  nnoremap <C-f> :NERDTreeFind<CR>
+  nnoremap <leader>f :NERDTreeFind<CR>
+
+  nnoremap <silent> <Leader>s :FixWhitespace<CR>
+  nnoremap <silent> <Leader>cc :Commentary<CR>
+
+  nnoremap <silent> <Leader>k :BookmarkShowAll<CR>
+  nnoremap <silent> <Leader>b :Unite -start-insert buffer<CR>
+  nnoremap <silent> bb :Unite -start-insert vim_bookmarks<CR>
+
+  nmap mm :BookmarkToggle<CR>
+  nmap mi :BookmarkAnnotate<CR>
+  nmap mn :BookmarkNext<CR>
+  nmap mp :BookmarkPrev<CR>
+  nmap ma :BookmarkShowAll<CR>
+  nmap mc :BookmarkClear<CR>
+  nmap mx :BookmarkClearAll<CR>
+  nmap mkk :BookmarkMoveUp
+  nmap mjj :BookmarkMoveDown
+
+  nmap <Leader>w :BufferClose<CR>
+  nmap <Leader>p :BufferPin<CR>
+  nmap <Leader>q :BufferCloseAllButPinned<CR>
+  nmap <C-q> :BufferCloseAllButCurrent<CR>
+
+  " Searching
+  let g:ackprg = 'ag --vimgrep'
+  let NERDTreeQuitOnOpen=1
+
+  nnoremap <C-s> :Ack!<Space>
+
+  set background=dark
+  set clipboard=unnamedplus
+  set completeopt=noinsert,menuone,noselect
+  set cursorline
+  set hidden
+  set inccommand=split
+  set mouse=a
+  set number
+  set relativenumber
+  set splitbelow splitright
+  set title
+  set ttimeoutlen=0
+  set wildmenu
+
+  set expandtab
+  set shiftwidth=2
+  set tabstop=2
+  filetype plugin indent on
+  syntax on
+
+  set termguicolors
+  set cursorline
+  let NERDTreeQuitOnOpen=1
+  let g:airline_powerline_fonts = 1
+  let g:airline#extensions#tabline#enabled = 1
+  let g:airline#extensions#tabline#buffer_nr_show = 1
+
+  set noswapfile
+
+  let g:bookmark_center = 1
+  let g:bookmark_location_list = 1
+  let g:bookmark_save_per_working_dir = 1
+  let g:bookmark_auto_save = 1
+
+
+]]
+
+
+vim.cmd("colorscheme dracula")
+vim.cmd("hi Normal ctermbg=none guibg=none")
